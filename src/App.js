1c8712750c0ba2c0ac6fb84b6eeb91dc31d79ccd
Ahmed Zribi <zribi.ahmed@perfectsoft.one>
2019-07-16 15:02:24 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+function Hats() {
+  return(
+    <div>
+      <h1>
+        Hats Page
+      </h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/hats" component={Hats} />
     </div>
   );
 }
